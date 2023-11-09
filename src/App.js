de3ba05424f9b3e5a1dc1e7ca033b6364caffc3f
anamulkarimkhan <makks_n@yahoo.com>
2023-11-10 10:15:03 +1100

    change heading

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Main from './Pages/Main';
+
 function App() {
   return (
+    <div>
+      <Main />
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -16,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+    </div>
     </div>
   );
 }
